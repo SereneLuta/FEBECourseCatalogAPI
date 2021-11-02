bdc7389947ef98c9b17a6637c1138d6cf5fe816f
SereneLuta <36087103+SereneLuta@users.noreply.github.com>
2021-11-02 09:37:52 -0400

    Initial commit

---
+++ Controllers/CourseCatalogController.cs
@@ -0,0 +1,27 @@
+﻿using CourseCatalogApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseCatalogApi.Controllers
+{
+    public class CourseCatalogController : ControllerBase
+    {
+        [HttpGet("course-catalog")]
+        public async Task<ActionResult<GetCourseCatalogResponse>> GetAllCourses()
+        {
+            var response = new GetCourseCatalogResponse
+            {
+                Data = new List<CourseCatalogResponseItem>
+                {
+                    new CourseCatalogResponseItem { Id = 1, Title="DevOps Essentials", Description="For Everyone!", NumberOfDays = 2},
+                    new CourseCatalogResponseItem { Id = 2, Title="DevOps For Service Developers", Description="For Developers", NumberOfDays = 2},
+                    new CourseCatalogResponseItem { Id = 3, Title="Developer Workshop", NumberOfDays = 1, Type=CourseType.Online}
+                }
+            };
+            return Ok(response);
+        }
+    }
+}
---
+++ Models/GetCourseCatalogResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseCatalogApi.Models
+{
+    public class GetCourseCatalogResponse
+    {
+        public IList<CourseCatalogResponseItem> Data { get; set; }
+    }
+
+    public enum CourseType { SelfPaced, InstructorLed=99, Online }
+    public class CourseCatalogResponseItem
+    {
+        public int Id { get; set; }
+        [Required][MaxLength(100)]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public int NumberOfDays { get; set; }
+
+        public CourseType Type { get; set; } = CourseType.InstructorLed;
+
+    }
+}
